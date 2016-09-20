4871becea8c3c800c735ae4e928549ebbb4d1b70
Brandon Arnold <br@ndonarnold.email>
2016-09-19 19:14:14 -0500

    Still in progress 1143 N-Choose-R Mod M problem

--- 1143_ncrModM_cpp/main.cpp
+++ 1143_ncrModM_cpp/main.cpp
@@ -2,11 +2,16 @@
 #include <math.h>
 using namespace std;
 
-int ncrModM(int n, int r, int m);
-int pFactorsInNFactorial(int n, int p);
+unsigned ncrModM(unsigned n, unsigned r, unsigned m);
+unsigned pFactorsInNFactorial(unsigned n, unsigned p);
+unsigned pFactorsAlt(unsigned n, unsigned p);
+void doSieve();
+const unsigned maxN = 1000000000;
 
 int main() {
-  int numProbs, n, r, m, result;
+  unsigned numProbs, n, r, m;
+  unsigned result;
+  doSieve();
   cin >> numProbs;
   while (numProbs--) {
     cin >> n;
@@ -18,58 +23,96 @@ int main() {
   return 0;
 }
 
-int ncrModM(int n, int r, int m) {
-  int ncrModM = 1;
-  int sqrtN = (int)sqrt(n);
+const int BLOCKSIZE =1<<16;
+const unsigned sqrtMaxN = 31622;
+bool C[BLOCKSIZE];
+unsigned Primes[50847534];   // primes to use in the sieve
+unsigned First[50847534];   // next integer divisible by p (delta form block start)
+unsigned NP = 0;
 
-  // perform sieve to sqrtN
-  bool* sieve = new bool[n + 1];
-  for(int i = 0; i < n + 1; i++) sieve[i] = true;
-  for(int i = 2; i <= sqrtN; i++)
-    if(sieve[i])
-      for (int j = i; j <= n / i; j++)
-        sieve[j * i] = false;
-
-  // get integer to primes mapping
-  int numPrimes = 0;
-  for(int i = 2; i < n+1; i++)
-    if(sieve[i]) numPrimes++;
-  int* nthPrime = new int[numPrimes];
-  int curPrime = 0;
-  for(int i = 2; i < n + 1; i++)
-    if(sieve[i])
-      nthPrime[curPrime++] = i;
-
-  // get prime factorization of n!/r!(n-r)!
-  int* nFact = new int[numPrimes];
-  for(int i = 0; i < numPrimes; i++) {
-    //add factorization of n!
-    nFact[i] = pFactorsInNFactorial(n, nthPrime[i]);
-
-    //subtract factorization of r!
-    nFact[i] -= pFactorsInNFactorial(r, nthPrime[i]);
-
-    //subtract factorization of (n-r)!
-    nFact[i] -= pFactorsInNFactorial(n - r, nthPrime[i]);
+void doSieve(){
+  for(unsigned p = 2; p <= sqrtMaxN; ++p)
+    if( !C[p] ) {
+      Primes[NP] = p;
+      First[NP] = p*p;
+      NP++;
+      for(unsigned n = p*p; n <= BLOCKSIZE; n+=p)
+        C[n] = 1;
+    }
+  for(unsigned B = sqrtMaxN + 1; B < maxN; B+=BLOCKSIZE) {
+    for(int j = 0; j < BLOCKSIZE; j++) {
+      C[j] = 0;
+    }
+    for(unsigned k = 0; k < NP; ++k) {
+      unsigned p = Primes[k];
+      unsigned t = First[k];
+      for( ; t < BLOCKSIZE; t+=p )
+        C[t] = 1;
+      First[k] = t-BLOCKSIZE;
+    }
+    for(unsigned c = 0; c < BLOCKSIZE; c++) {
+      if( !C[c] ) {
+        unsigned p = B + c;
+        Primes[NP] = p;
+        First[NP] = p*p;
+        if(NP == 3402) {
+          cout << NP;
+          cout << " ";
+          cout << p << endl;
+        }
+        NP++;
+        for(unsigned n = p*p; p <= BLOCKSIZE; n+=p)
+          C[n] = 1;
+      }
+    }
   }
+}
 
-  // perform mod of M by reduction technique
-  for(int i = 0; i < numPrimes; i++)
-    ncrModM = (ncrModM * (int)pow(nthPrime[i], nFact[i])) % m;
-
-  delete [] sieve;
-  delete [] nthPrime;
-  delete [] nFact;
+unsigned ncrModM(unsigned n, unsigned r, unsigned m) {
+  unsigned ncrModM = 1;
+  unsigned p;
+  unsigned nfPower;
+  for(unsigned i = 0; (i < 50847534 && Primes[i] <= n); i++) {
+    p = Primes[i];
+    cout << p;
+    if(p < 1) break;
+    nfPower = pFactorsInNFactorial(n, p);
+    nfPower -= pFactorsInNFactorial(r, p);
+    nfPower -= pFactorsInNFactorial(n - r, p);
+    if(nfPower == 0) continue;
+    p = (int)pow(p, nfPower);
+    cout << ' ';
+    cout << p;
+    cout << ' ';
+    cout << ncrModM;
+    ncrModM *= p % m;
+    ncrModM %= m;
+    cout << ' ';
+    cout << ncrModM;
+    cout << ' ';
+    cout << m << endl;
+    if(ncrModM <= 0) break;
+    if(nfPower < 0) break;
+  }
   return ncrModM;
 }
 
-int pFactorsInNFactorial(int n, int p) {
-  int baseNRemainder = n,
-    maxPow = (int)floor(log(n)/log(p)),
+unsigned pFactorsInNFactorial(unsigned n, unsigned p) {
+  unsigned sum = 0, curP = p;
+  for(unsigned i = 1; curP <= n; i++) {
+    sum += n / curP;
+    curP *= p;
+  }
+  return sum;
+}
+
+unsigned pFactorsAlt(unsigned n, unsigned p) {
+  unsigned baseNRemainder = n,
+    maxPow = (unsigned)(log(n)/log(p)),
     sumExp = 0;
   while(baseNRemainder > 0) {
-    int curPPow = (int)pow(p, maxPow--);
-    int curCoeff = baseNRemainder / curPPow;
+    unsigned curPPow = (unsigned)pow(p, maxPow--);
+    unsigned curCoeff = baseNRemainder / curPPow;
     sumExp += curCoeff;
     baseNRemainder -= curCoeff * curPPow;
   }
